f6b4e034a55081f67dfda66d6ebc082e651da6c9
Alexander Ulanov <nashb@yandex.ru>
2016-01-11 18:36:11 -0800

    Implement power of 2 step. Small fix to time measurement

--- src/bidmat.scala
+++ src/bidmat.scala
@@ -1,6 +1,7 @@
 val range = Seq(1, 10, 100, 1000, 10000)
+
 // float in GPU
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = grand(i, i); val b = grand(i, j); System.gc(); val t = System.nanoTime(); val c = a * b; c.toFMat(null); println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
+for (i <- range) { for(j <- range) { if (j > i) {} else { val a = grand(i, i); val b = grand(i, j); System.gc(); val t = System.nanoTime(); val c = a * b; println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
 
 // float back and forth
 for (i <- range) { for(j <- range) { if (j > i) {} else { val a = rand(i, i); val b = rand(i, j); System.gc(); val t = System.nanoTime(); val ga = gzeros(i, i); val gb = gzeros(i, j); ga.copyFrom(a); gb.copyFrom(b); val gres = ga * gb; val c = zeros(i, j); gres.toFMat(c); println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
@@ -10,3 +11,10 @@ for (i <- range) { for(j <- range) { if (j > i) {} else { val a = rand(i, i); va
 
 // double CPU
 for (i <- range) { for(j <- range) { if (j > i) {} else { val a = drand(i, i); val b = drand(i, j); System.gc(); val t = System.nanoTime(); val c = a * b;println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
+
+
+// With power 2 step and square matrices
+val range = for (b  <- 0 to 12) yield { math.pow(2, b).toInt}
+
+// float in GPU
+for (i <- range) { val a = grand(i, i); val b = grand(i, i); System.gc(); val t = System.nanoTime(); val c = a * b; val totalTime = System.nanoTime() - t; println(i + "x" + i + "*" + i + "x" + i + "\t" + totalTime / 1e9)}
