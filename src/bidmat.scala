0277e2466ac0c7933824d8b115420bb6cfab3041
Alexander Ulanov <nashb@yandex.ru>
2016-01-20 17:41:09 -0800

    Averaging and Flops

--- src/bidmat.scala
+++ src/bidmat.scala
@@ -1,20 +1,29 @@
-val range = Seq(1, 10, 100, 1000, 10000)
-
-// float in GPU
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = grand(i, i); val b = grand(i, j); System.gc(); val t = System.nanoTime(); val c = a * b; println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
-
-// float back and forth
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = rand(i, i); val b = rand(i, j); System.gc(); val t = System.nanoTime(); val ga = gzeros(i, i); val gb = gzeros(i, j); ga.copyFrom(a); gb.copyFrom(b); val gres = ga * gb; val c = zeros(i, j); gres.toFMat(c); println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
-
-// float CPU
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = rand(i, i); val b = rand(i, j); System.gc(); val t = System.nanoTime(); val c = a * b;println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
-
-// double CPU
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = drand(i, i); val b = drand(i, j); System.gc(); val t = System.nanoTime(); val c = a * b;println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
-
-
-// With power 2 step and square matrices
-val range = for (b  <- 0 to 12) yield { math.pow(2, b).toInt}
-
-// float in GPU
-for (i <- range) { val a = grand(i, i); val b = grand(i, i); System.gc(); val t = System.nanoTime(); val c = a * b; val totalTime = System.nanoTime() - t; println(i + "x" + i + "*" + i + "x" + i + "\t" + totalTime / 1e9)}
+// With some step, square matrices and multiple runs
+val minDim = 1000
+val maxDim = 64000
+val step = 1000
+val repetitions = 10
+val range = for (b <- minDim to maxDim by step) yield b
+//val range = for (b  <- 6 to 14) yield { math.pow(2, b).toInt}
+for (i <- 0 until range.length) {
+  var result = new Array[Double](repetitions)
+  val dim = range(i) 
+  for (j <- 0 until repetitions) {
+    val a = grand(dim, dim); 
+    val b = grand(dim, dim); 
+    System.gc(); 
+    val t = System.nanoTime(); 
+    val c = a * b; 
+    val totalTime = System.nanoTime() - t; 
+    result(j) = totalTime; 
+    a.free
+    b.free
+    c.free
+  }
+  val someValue = result.last
+  scala.util.Sorting.quickSort(result)
+  val median = (result(repetitions / 2) + result(repetitions / 2 - 1)) / 2
+  val avg = result.sum / result.length
+  val gigaFlops = 2.0 / median * dim * dim * dim
+  println(dim + "\t" + median / 1e9 + "\t" + avg / 1e9 + "\t" + someValue / 1e9 + "\t" + gigaFlops)
+}
--- src/breeze.scala
+++ src/breeze.scala
@@ -1,23 +1,12 @@
-val range = Seq(1, 10, 100, 1000, 10000)
-// Float
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = breeze.linalg.DenseMatrix.rand[Double](i, i).mapValues(_.toFloat); val b = breeze.linalg.DenseMatrix.rand[Double](i, j).mapValues(_.toFloat); System.gc(); val t = System.nanoTime(); val c = a * b; println(c(0,0) + " " + i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
-// Double
-for (i <- range) { for(j <- range) { if (j > i) {} else { val a = breeze.linalg.DenseMatrix.rand[Double](i, i); val b = breeze.linalg.DenseMatrix.rand[Double](i, j); System.gc(); val t = System.nanoTime(); val c = a * b; println(i + "x" + i + "*" + i + "x" + j + "\t" + (System.nanoTime() - t) / 1e9)}}}
-
-// With power 2 step and square matrices
-val range = for (b  <- 0 to 14) yield { math.pow(2, b).toInt}
-// Float
-for (i <- range) { val a = breeze.linalg.DenseMatrix.rand[Double](i, i).mapValues(_.toFloat); val b = breeze.linalg.DenseMatrix.rand[Double](i, i).mapValues(_.toFloat); System.gc(); val t = System.nanoTime(); val c = a * b; val totalTime = System.nanoTime() - t; println(c(0,0) + " " + i + "x" + i + "*" + i + "x" + i + "\t" + totalTime / 1e9)}
-// Double
-for (i <- range) { val a = breeze.linalg.DenseMatrix.rand[Double](i, i); val b = breeze.linalg.DenseMatrix.rand[Double](i, i); System.gc(); val t = System.nanoTime(); val c = a * b; val totalTime = System.nanoTime() - t; println(i + "x" + i + "*" + i + "x" + i + "\t" + totalTime / 1e9)}
-
-// With 128 step, square matrices and multiple runs
-val maxDim = 4096
+// With some step, square matrices and multiple runs
+val minDim = 1000
+val maxDim = 46000
+val step = 1000
 val repetitions = 10
-val range = for (b <- 128 until maxDim by 128) yield b
-var result = new Array[Double](repetitions)
-println("dim\tmean\tavg")
+val range = for (b <- minDim to maxDim by step) yield b
+//val range = for (b  <- 6 to 14) yield { math.pow(2, b).toInt}
 for (i <- 0 until range.length) {
+  var result = new Array[Double](repetitions)
   val dim = range(i) 
   for (j <- 0 until repetitions) {
     val a = breeze.linalg.DenseMatrix.rand[Double](dim, dim).mapValues(_.toFloat); 
@@ -25,9 +14,13 @@ for (i <- 0 until range.length) {
     System.gc(); 
     val t = System.nanoTime(); 
     val c = a * b; 
-    result(j) += (System.nanoTime() - t) / repetitions / 1e9; 
+    val totalTime = System.nanoTime() - t; 
+    result(j) = totalTime; 
   }
-  val mean = (result(repetitions / 2) + result(repetitions / 2 - 1)) / 2
+  val someValue = result.last
+  scala.util.Sorting.quickSort(result)
+  val median = (result(repetitions / 2) + result(repetitions / 2 - 1)) / 2
   val avg = result.sum / result.length
-  println(dim + "\t" + mean + "\t" + avg)
+  val gigaFlops = 2.0 / median * dim * dim * dim
+  println(dim + "\t" + median / 1e9 + "\t" + avg / 1e9 + "\t" + someValue / 1e9 + "\t" + gigaFlops)
 }
